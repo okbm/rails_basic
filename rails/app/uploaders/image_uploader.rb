6dc35435ebfd2ae3cc2f15352d2bcb6bd3c8ee5c
okbm <masayoshiokabe.321@gmail.com>
2018-10-01 14:53:08 +0900

    feat: carrierwaveの設定とmodelの追加

---
+++ rails/app/uploaders/image_uploader.rb
@@ -0,0 +1,23 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads/cache/#{model.class.to_s.underscore}/#{model.id}"
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(pdf doc docx docm xls xlsx xlsm zip jpg ppt pptx)
+  end
+
+  def fog_attributes
+    { 'x-amz-server-side-encryption': 'AES256' }
+  end
+
+  def asset_host
+    "#{Rails.root}" + '/public' if Rails.env.test? || (Rails.env.development? && storage.uploader.mounted_as == :file)
+  end
+end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_01_054236) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
